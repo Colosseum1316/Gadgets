1a118bb707d8a06e47385dea5e19c103207f34ff
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2025-12-28 15:28:24 +0800

    Add VoidDummy

--- VoidDummy/src/main/java/colosseum/voiddummy/VoidDummy.java
+++ VoidDummy/src/main/java/colosseum/voiddummy/VoidDummy.java
@@ -1,19 +1,25 @@
 package colosseum.voiddummy;
 
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.RemoteConsoleCommandSender;
 import org.bukkit.event.HandlerList;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Random;
 
-public final class VoidDummy extends JavaPlugin {
+public final class VoidDummy extends JavaPlugin implements CommandExecutor {
 
     private final VoidDummyEventListener eventListener = new VoidDummyEventListener();
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(eventListener, this);
+        getCommand("voiddummy").setExecutor(this);
     }
 
     @Override
@@ -29,5 +35,14 @@ public final class VoidDummy extends JavaPlugin {
                 return createChunkData(world);
             }
         };
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof ConsoleCommandSender) && !(sender instanceof RemoteConsoleCommandSender)) {
+            return false;
+        }
+        getServer().getPluginManager().disablePlugin(this);
+        return true;
     }
 }
