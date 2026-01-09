013b9e1ed06dedc42e46b96bd72204a08ae48c17
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2026-01-09 14:11:19 +0800

    add

--- LegacyBossBar/src/main/java/colosseum/legacybossbar/LegacyBossBar.java
+++ LegacyBossBar/src/main/java/colosseum/legacybossbar/LegacyBossBar.java
@@ -1,59 +1,33 @@
 package colosseum.legacybossbar;
 
-import net.minecraft.server.v1_8_R3.EntityWither;
-import net.minecraft.server.v1_8_R3.PacketPlayOutEntityDestroy;
-import net.minecraft.server.v1_8_R3.PacketPlayOutSpawnEntityLiving;
-import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
 
-public final class LegacyBossBar extends JavaPlugin implements Listener {
+public final class LegacyBossBar extends JavaPlugin {
+
+    private static LegacyBossBar instance;
+
+    public static JavaPlugin getInstance() {
+        return instance;
+    }
+
+    private WitherManager witherManager;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
+        witherManager = new WitherManager();
+        witherManager.activate();
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerJoin(PlayerJoinEvent e) {
-        Player player = e.getPlayer();
-        EntityWither wither = new EntityWither(((CraftWorld) player.getWorld()).getHandle());
-        wither.setGoalTarget(null);
-        wither.setInvisible(true);
-        wither.setCustomName(player.getName());
-        wither.setCustomNameVisible(true);
-        wither.getDataWatcher().watch(8, (byte) 0);
-        wither.getDataWatcher().watch(15, (byte) 1);
-        wither.getDataWatcher().watch(17, 0);
-        wither.getDataWatcher().watch(18, 0);
-        wither.getDataWatcher().watch(19, 0);
-        wither.getDataWatcher().watch(20, 0);
-        BukkitTask task = getServer().getScheduler().runTaskTimerAsynchronously(this, () -> {
-            Location loc = player.getLocation();
-            wither.setLocation(loc.getX(), loc.getY() - 3, loc.getZ(), loc.getYaw(), loc.getPitch());
-            PacketPlayOutSpawnEntityLiving packet = new PacketPlayOutSpawnEntityLiving(wither);
-            ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
-        }, 0, 5L);
-        getServer().getPluginManager().registerEvents(new Listener() {
-            @EventHandler(priority = EventPriority.MONITOR)
-            public void onPlayerQuit(PlayerQuitEvent e) {
-                if (!e.getPlayer().equals(player)) {
-                    return;
-                }
-                HandlerList.unregisterAll(this);
-                getServer().getScheduler().cancelTask(task.getTaskId());
-                PacketPlayOutEntityDestroy packet = new PacketPlayOutEntityDestroy(wither.getId());
-                ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
-            }
-        }, this);
+    @Override
+    public void onDisable() {
+        if (witherManager != null) {
+            witherManager.deactivate();
+        }
+        witherManager = null;
     }
 }
---
+++ LegacyBossBar/src/main/java/colosseum/legacybossbar/WitherManager.java
@@ -0,0 +1,111 @@
+package colosseum.legacybossbar;
+
+import net.minecraft.server.v1_8_R3.EntityWither;
+import net.minecraft.server.v1_8_R3.PacketPlayOutEntityDestroy;
+import net.minecraft.server.v1_8_R3.PacketPlayOutEntityTeleport;
+import net.minecraft.server.v1_8_R3.PacketPlayOutSpawnEntityLiving;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public final class WitherManager implements Listener {
+    private final Map<UUID, Integer> activeWithers = new HashMap<>();
+    private BukkitTask withersTeleportTask;
+
+    public void activate() {
+        Bukkit.getPluginManager().registerEvents(this, LegacyBossBar.getInstance());
+        withersTeleportTask = Bukkit.getScheduler().runTaskTimer(LegacyBossBar.getInstance(), () -> {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                int eid = activeWithers.getOrDefault(player.getUniqueId(), -1);
+                if (eid == -1) {
+                    continue;
+                }
+                Location loc = player.getLocation().add(0, -30, 0);
+                PacketPlayOutEntityTeleport packet = new PacketPlayOutEntityTeleport(eid, (int) (loc.getX() * 32), (int) (loc.getY() * 32.0), (int) (loc.getZ() * 32.0), (byte) 0, (byte) 0, false);
+                ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+            }
+        }, 0, 1L);
+    }
+
+    public void deactivate() {
+        withersTeleportTask.cancel();
+        activeWithers.clear();
+        HandlerList.unregisterAll(this);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        if (e.getPlayer().isDead()) {
+            return;
+        }
+        spawnWither(e.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        despawnWither(e.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerMove(PlayerChangedWorldEvent e) {
+        despawnWither(e.getPlayer());
+        spawnWither(e.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerRespawn(PlayerRespawnEvent e) {
+        Player player = e.getPlayer();
+        despawnWither(player);
+        Bukkit.getScheduler().runTaskLater(LegacyBossBar.getInstance(), () -> {
+            if (player.isOnline()) {
+                spawnWither(player);
+            }
+        }, 20L);
+    }
+
+    private void spawnWither(Player player) {
+        if (activeWithers.containsKey(player.getUniqueId())) {
+            return;
+        }
+        final EntityWither wither = new EntityWither(((CraftWorld) player.getWorld()).getHandle());
+        wither.setGoalTarget(null);
+        wither.setInvisible(true);
+        wither.setCustomName(player.getName());
+        wither.setCustomNameVisible(true);
+        wither.getDataWatcher().watch(8, (byte) 0);
+        wither.getDataWatcher().watch(15, (byte) 1);
+        wither.getDataWatcher().watch(17, 0);
+        wither.getDataWatcher().watch(18, 0);
+        wither.getDataWatcher().watch(19, 0);
+        wither.getDataWatcher().watch(20, 0);
+        Location loc = player.getLocation();
+        wither.setLocation(loc.getX(), loc.getY() - 30, loc.getZ(), loc.getYaw(), loc.getPitch());
+        PacketPlayOutSpawnEntityLiving packet = new PacketPlayOutSpawnEntityLiving(wither);
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+        activeWithers.put(player.getUniqueId(), wither.getId());
+    }
+
+    private void despawnWither(Player player) {
+        Integer id = activeWithers.remove(player.getUniqueId());
+        if (id == null || id == -1) {
+            return;
+        }
+        PacketPlayOutEntityDestroy packet = new PacketPlayOutEntityDestroy(id);
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+    }
+}
